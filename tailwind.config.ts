2907d23038dcd11eb4b05c888b46542205cb8bfe
Wave Gerência <wavegerenciaofc@gmail.com>
2025-09-03 12:31:46 +0000

    Quero a font futura em textos que não são titulos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
+        body: ['Futura', 'sans-serif'],
         headline: ['Space Grotesk', 'sans-serif'],
         code: ['monospace'],
       },
