ef35b2d4bb1ce044af7eab4c6a85ff67e4a81f4d
Wave Gerência <wavegerenciaofc@gmail.com>
2025-09-08 19:38:28 +0000

    mudança 30%

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 🔑 Isso habilita o static export
   typescript: {
     ignoreBuildErrors: true,
   },
